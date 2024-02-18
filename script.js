50507c89da8c560be84b6a66ceb2361a70a9cb0a
JesperWhendin <143404976+JesperWhendin@users.noreply.github.com>
2024-02-18 11:46:17 +0100

    General improvements for services.

--- script.js
+++ script.js
@@ -7,27 +7,19 @@ class Product {
   }
 }
 
-const services = [
-    new Product("Test", "ServiceExample.jpg", 10, "A delightful service."),
-    new Product("Service 1", "ServiceExample.jpg", 26, "Example service description 1."),
-    new Product("Service 2", "ServiceExample.jpg", 39, "Example service description 2."),
-    new Product("Service 3", "ServiceExample.jpg", 74, "Example service description 3."),
-    new Product("Service 4", "ServiceExample.jpg", 119, "Example service description 4."),
-    new Product("Service 5", "ServiceExample.jpg", 189, "Example service description 5.")
+const products = [
+  new Product("Bilvård Basic", "ServiceExample.jpg", 790, "Ett grundläggande bilvårdspaket inklusive tvätt, invändig dammsugning och fönsterputs."),
+  new Product("Bilvård Medel", "ServiceExample.jpg", 1590, "Ett omfattande bilvårdspaket som inkluderar Bilvård Basic och även poleringstjänst som återställer glansen på lacken och ger bilen en skyddande yta."),
+  new Product("Bilvård Premium", "ServiceExample.jpg", 3490, "En djupgående rengöring av bilen som inkluderar Bilvård Medel inklusive städning av tyger, rengöring av instrumentbräda och behandling av läder."),
+  new Product("Keramisk Lackskyddsbehandling", "ServiceExample.jpg", 5990, "En högteknologisk behandling som ger ett långvarigt skydd mot smuts, vägsalt och UV-strålar."),
+  new Product("Motorrengöring", "ServiceExample.jpg", 1290, "En professionell rengöring av motorn för att avlägsna smuts och förlänga dess livslängd."),
+  new Product("Turbotvätt", "ServiceExample.jpg", 199, "En snabb och effektiv behandling för att avlägsna asfalt, insekter och andra rester från bilens lack, vindruta och fälgar.")
 ];
 
-const serviceList = document.querySelector(".service-list");
+const productList = document.querySelector(".product-list");
     let i = 1;
 function listServices() {
-  for (const prod of services) {
-    console.log(prod.name);
-    console.log(prod.image);
-    console.log(prod.price);
-    console.log(prod.description);
-
-
-    console.log(i)
-    i+++1;
+  for (const prod of products) {
     const col = document.createElement("div");
     const card = document.createElement("div");
     const body = document.createElement("div");
@@ -35,20 +27,33 @@ function listServices() {
     const img = document.createElement("img");
     const cost = document.createElement("p");
     const desc = document.createElement("p");
+    const addBtn = document.createElement("button");
 
+    card.setAttribute("class", "card product");
     title.innerText = prod.name;
-    cost.innerText = prod.price;
+    title.setAttribute("class", "title text-center");
+    cost.innerText = prod.price+++":-";
+    
     desc.innerText = prod.description;
     img.setAttribute("src", prod.image);
     img.setAttribute("alt", prod.name);
+    img.setAttribute("class", "product-image");
+    addBtn.setAttribute("type", "button");
+    addBtn.setAttribute("class", "btn btn-secondary");
+    addBtn.innerText = ("Lägg till");
 
-    serviceList.appendChild(col);
+    addBtn.onclick = () => {
+      addToCart(prod)
+    };
+
+    productList.appendChild(col);
     col.appendChild(card);
+    card.appendChild(img);
     card.appendChild(body);
     body.appendChild(title);
-    body.appendChild(cost);
     body.appendChild(desc);
-    card.appendChild(img);
+    body.appendChild(cost);
+    card.appendChild(addBtn);
   }
 }
 
@@ -56,9 +61,8 @@ document.addEventListener("DOMContentLoaded", function() {
     listServices();
 });
 
-
-
 let cart = [];
 
-
-// listServices();
+function addToCart(product){
+  cart.push(product);
+}
